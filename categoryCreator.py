44eac3cb91abf1d8b57c401aece0b982d26b05a7
Doug Mello <dmello88@gmail.com>
2017-12-17 21:48:52 -0500

    finished showCategories & showItems methods.

--- categoryCreator.py
+++ categoryCreator.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from database_setup import Restaurant, Base, MenuItem
+from database_setup import Base, Category, Item, User
 
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///itemcatalog.db')
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
@@ -18,206 +18,135 @@ DBSession = sessionmaker(bind=engine)
 # session.rollback()
 session = DBSession()
 
-
-# Menu for UrbanBurger
-category1 = Category(name="")
-
-session.add(restaurant1)
+User1 = User(name="Doug Mello", email="dmello88@gamil.com")
+session.add(User1)
 session.commit()
 
-item1 = Item(name="", description="",category=category1)
+category1 = Category(user_id=1, name="Shirts")
 
-session.add(menuItem2)
+session.add(category1)
 session.commit()
 
+item1 = Item(user_id=1, name="T-Shirt", description="Cotton & short sleeves",
+    category=category1)
 
-item2 = Item(name="", description="",category=category1)
-
-session.add(menuItem1)
-session.commit()
-
-item3 = Item(name="", description="",category=category1)
-
-session.add(menuItem2)
-session.commit()
-
-item4 = Item(name="", description="",category=category1)
-
-session.add(menuItem3)
-session.commit()
-
-item5 = Item(name="", description="",category=category1)
-
-session.add(menuItem4)
+session.add(item1)
 session.commit()
 
 
-# Menu for Super Stir Fry
-category2 = Category(name="")
+item2 = Item(user_id=1, name="Polo shirt",
+    description="Short sleeves & a collar", category=category1)
+
+session.add(item2)
+session.commit()
+
+item3 = Item(user_id=1, name="Button Up", description="Long sleeve and buttons",
+    category=category1)
+
+session.add(item3)
+session.commit()
+
+item4 = Item(user_id=1, name="Sleeveless shirt",
+    description="Cotton & no sleeves.", category=category1)
+
+session.add(item4)
+session.commit()
+
+
+
+category2 = Category(user_id=1, name="Pants")
 
 session.add(category2)
 session.commit()
 
 
-item1 = Item(name="", description="",category=category2)
+item1 = Item(user_id=1, name="Jeans", description="Blue jeans",
+    category=category2)
 
 session.add(item1)
 session.commit()
 
-item2= Item(name="", description="",category=category2)
+item2= Item(user_id=1, name="Kackis", description="Brown cotton pants",
+    category=category2)
 
 session.add(item2)
 session.commit()
 
-item3 = Item(name="", description="",category=category2)
+item3 = Item(user_id=1, name="Shorts",
+    description="Cotton and cut at the knees.", category=category2)
 
 session.add(item3)
 session.commit()
 
-item4 = Item(name="", description="",category=category2)
+item4 = Item(user_id=1, name="Parachute pants", description="Whoah!",
+    category=category2)
 
 session.add(item4)
 session.commit()
 
-item5 = Item(name="", description="",category=category2)
-
-session.add(item5)
-session.commit()
 
 
-# Menu for Panda Garden
-category3 = Category(name="")
+category3 = Category(user_id=1, name="Hats")
 
 session.add(category3)
 session.commit()
 
 
-item1 = Item(name="", description="",category=category3)
+item1 = Item(user_id=1, name="Blue hat", description="It's blue.",
+    category=category3)
 
 session.add(item1)
 session.commit()
 
-item2 = Item(name="", description="",category=category3)
+item2 = Item(user_id=1, name="Red hat", description="It's red.",
+    category=category3)
 
 session.add(item2)
 session.commit()
 
-item3 = Item(name="", description="",category=category3)
+item3 = Item(user_id=1, name="Green hat", description="It's green.",
+    category=category3)
 
 session.add(item3)
 session.commit()
 
-item4 = Item(name="", description="",category=category3)
+item4 = Item(user_id=1, name="Black hat", description="It's black.",
+    category=category3)
 
 session.add(item4)
 session.commit()
 
-item5 = Item(name="", description="",category=category3)
 
-session.add(item5)
-session.commit()
-
-
-# Menu for Thyme for that
-category4 = Category(name="")
+category4 = Category(user_id=1, name="Jackets")
 
 session.add(category4)
 session.commit()
 
 
-item1 = Item(name="", description="", category=category4)
+item1 = Item(user_id=1, name="Jean jacket", description="It's not the 80's.",
+    category=category4)
 
 session.add(item1)
 session.commit()
 
-item2 = Item(name="", description="", category=category4)
+item2 = Item(user_id=1, name="Windbreaker", description="Breaks the wind",
+    category=category4)
 
 session.add(item2)
 session.commit()
 
-item3 = Item(name="", description="", category=category4)
+item3 = Item(user_id=1, name="Winter jacket", description="Warm!",
+    category=category4)
 
 session.add(item3)
 session.commit()
 
-item4 = Item(name="", description="", category=category4)
+item4 = Item(user_id=1, name="Sports jacket", description="Fancy!",
+    category=category4)
 
 session.add(item4)
 session.commit()
 
-item5 = Item(name="", description="", category=category4)
-
-session.add(item5)
-session.commit()
 
 
-# Menu for Tony's Bistro
-category5 = Category(name="")
-
-session.add(category5)
-session.commit()
-
-
-item1 = Item(name="", description="", category=category5)
-
-session.add(menuItem1)
-session.commit()
-
-item2 = Item(name="", description="", category=category5)
-
-session.add(item2)
-session.commit()
-
-item3 = Item(name="", description="", category=category5)
-
-session.add(item3)
-session.commit()
-
-item4 = Item(name="", description="", category=category5)
-
-session.add(item4)
-session.commit()
-
-item5 = Item(name="", description="", category=category5)
-
-session.add(item5)
-session.commit()
-
-
-# Menu for Andala's
-category6 = Category(name="")
-
-session.add(category6)
-session.commit()
-
-
-item1 = Item(name="", description="", category=category6)
-
-session.add(item1)
-session.commit()
-
-item2 = Item(name="", description="", category=category6)
-
-session.add(item2)
-session.commit()
-
-item3 = Item(name="", description="", category=category6)
-
-session.add(item3)
-session.commit()
-
-item4 = Item(name="", description="", category=category6)
-
-session.add(item4)
-session.commit()
-
-item5 = Item(name="", description="", category=category6)
-
-session.add(item5)
-session.commit()
-
-
-
-
-
-print "added menu items!"
+print "added category items!"
--- project.py
+++ project.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func, desc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, User, Category, Item
 from flask import session as login_session
@@ -34,11 +34,27 @@ session = DBSession()
 
 # 4. Google login method - UNFINISHED & UNTESTED
 
-# 5.  Method for created a user. - UNFINISHED & UNTESTED
+# 5.  Method for created a user. - UNTESTED
+def createUser(login_session):
+    newUser = User(name=login_session['username'], email=login_session[
+                   'email'], picture=login_session['picture'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email=login_session['email']).one()
+    return user.id
 
-# 6. Method for getting user info. - UNFINISHED & UNTESTED
+# 6. Method for getting user info. -  UNTESTED
+def getUserInfo(user_id):
+    user = session.query(User).filter_by(id=user_id).one()
+    return user
 
-# 7. Method for getting user ID. - UNFINISHED & UNTESTED
+# 7. Method for getting user ID. - UNTESTED
+def getUserID(email):
+    try:
+        user = session.query(User).filter_by(email=email).one()
+        return user.id
+    except:
+        return None
 
 # 8. Method for dissconnecting from google sign in. - UNFINISHED & UNTESTED
 
@@ -46,32 +62,45 @@ session = DBSession()
 
 # 10. Method to get JSON APIs of the items in a certain category. - UNFINISHED & UNTESTED
 
-# 11.  Method to show all categorires & latest items added. - UNTESTED
+# 11.  Method to show all categorires & latest items added.
 @app.route('/')
 @app.route('/catalog/')
 def showCategories():
-    categories = session.query(Category).order_by((Category.name))
+    categories = session.query(Category).order_by(Category.name)
+    itemCount = session.query(func.count(Item.id))
+    print itemCount
+    recentItems = session.query(Item.name, Category.name).\
+        join(Item.category).order_by(Item.id.desc()).limit(5)
+    print recentItems
     if 'username' not in login_session:
-        return render_template('publicCategories.html', categories=categories)
+        return render_template('publicCategories.html', categories=categories,
+            items=recentItems)
     else:
-        return render_template('categories.html', categories=categories)
+        return render_template('categories.html', categories=categories,
+            items=recentItems)
 
 # 12. Method to show all items in a category - UNTESTED
-@app.route('/catalog/<int:category_id>/items')
-def showCategory(category_id):
-    category = session.query(Category).filter_by(id=category_id).one()
-    creator = getUserInfo(category.user_id)
-    items = session.query(Item).filter_by(category_id=category_id).all()
+@app.route('/catalog/<current_category>/items')
+def showCategory(current_category):
+    categories = session.query(Category).order_by(Category.name)
+    currentCategory = session.query(Category).filter_by\
+        (name = current_category.title())
+    for i in currentCategory:
+        categoryID = i.id
+        categoryUserID = i.user_id
+    creator = getUserInfo(categoryUserID)
+    items = session.query(Item).filter_by(category_id=categoryID)
     if 'username' not in login_session or creator.id != login_session['user_id']:
         return render_template('publicCategory.html', items=items,
-            category=category, creator=creator)
+        creator=creator, category=currentCategory, categories=categories)
     else:
-        return render_templates('category.html', items=items, category=category,
-            creator=creator)
+        return render_templates('category.html', items=items, creator=creator,
+            category=currentCategory, categories=categories)
+
 
 # 13. Method to add an item. - UNFINISHED & UNTESTED
-@app.route('/catalog/<int:category_id>/new', methods=['GET', 'POST'])
-def newItem(category_id):
+@app.route('/catalog/<current_category>/new', methods=['GET', 'POST'])
+def newItem(current_category):
     category = session.query(Category).filter_by(id=category_id).one()
     if 'username' not in login_session:
         return redirect('/login')
@@ -88,17 +117,16 @@ def newItem(category_id):
             session.add(newItem)
             session.commit()
             flash('New Menu %s Item Successfully Created' % (newItem.name))
-            return redirect(url_for('showCategory', category_id=category_id))
+            return redirect(url_for('showCategory', current_category=name))
         else:
             return render_template('addItem.html', category_id=category_id)
 
 # 14. Method to show description of a certain itme. - UNTESTED
-@app.route('/catalog/<int:category_id>/<int:item_id>')
-def showItem(category_id, item_id):
+@app.route('/catalog/<current_category>/<current_item>')
+def showItem(current_category, current_item):
     category = session.query(Category).filter_by(id=category_id).one()
     item = session.query(Item).filter_by(id=item_id).one()
-    if 'username' not in login_session or creator.id !=
-        login_session['user_id']:
+    if 'username' not in login_session or creator.id != login_session['user_id']:
         return render_template('publicItem.html', item=item,
             description=description, creator=creator)
     else:
@@ -106,8 +134,8 @@ def showItem(category_id, item_id):
             creator=creator)
 
 # 15. Method to edit an item name & description.- UNTESTED
-@app.route('catalog/<int:category_id>/<int:item_id>/edit')
-def editItem(category_id, item_id):
+@app.route('/catalog/<current_category>/<current_item>/edit')
+def editItem(current_category, current_item):
     if 'username' not in login_session:
         return redirect('/login')
     category = session.query(Category).filter_by(id=category_id).one()
@@ -132,8 +160,8 @@ def editItem(category_id, item_id):
             item_id=item_id, item=editedItem)
 
 # 16. Method to delete an item. - UNTESTED
-@app.route('catalog/<int:category_id>/<int:item_id>/delete')
-def deleteItem(category_id, item_id):
+@app.route('/catalog/<current_category>/<current_item>/delete')
+def deleteItem(current_category, current_item):
     if 'username' not in login_session:
         return redirect('/login')
     category = session.query(Category).filter_by(id=category_id).one()
